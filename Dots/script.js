9b29994ebc1169169d8913e67888a26fb1b8b604
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-08-21 18:35:59 +0100

    added generate button to Dots Application

--- Dots/script.js
+++ Dots/script.js
@@ -1,16 +1,25 @@
+window.onload = function() {
+  var btnGenerate = document.getElementById("btnGenerate");
+  btnGenerate.onclick = generateDots;
+}
 var canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight + 2;
 
 var c = canvas.getContext("2d");
 
-for (var i = 0; i < 200; i++) {
-  var colours = ["C44536", "EDDDD4", "772E25", "197278", "283D3B"];
-  var radius = Math.random() * 40;
-  var x = Math.random() * canvas.width;
-  var y = Math.random() * canvas.height;
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.fillStyle = "#" + colours[Math.floor(Math.random() * colours.length)];
-  c.fill();
+
+
+function generateDots() {
+  c.clearRect(0,0, canvas.width, canvas.height);
+  for (var i = 0; i < 200; i++) {
+    var colours = ["C44536", "EDDDD4", "772E25", "197278", "283D3B"];
+    var radius = Math.random() * 40;
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
+    c.beginPath();
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
+    c.fillStyle = "#" + colours[Math.floor(Math.random() * colours.length)];
+    c.fill();
+  }
 }
