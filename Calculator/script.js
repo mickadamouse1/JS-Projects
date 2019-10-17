4bb79a115d346842736ed5ebbb85adb0d6289d53
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-10-17 20:20:54 +0100

    Calculator: Completed Virtual Calculator Application

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,13 +1,5 @@
 /////////////////////////////////////////////////////////////////////
 
-// SESSION NOTES
-
-// Adding single digits works. However, it doesnt allow us to add double digits.
-// We need to store digits and operate on "=" rather than operating when a number is clicked.
-// Good luck!
-
-/////////////////////////////////////////////////////////////////////
-
 // Creates global variables
 
 var outputScreen = document.getElementById("outputScreen");
@@ -26,8 +18,10 @@ var btnClear = document.getElementById("btnClear");
 var btnNumArr = [btnNum0, btnNum1, btnNum2, btnNum3, btnNum4, btnNum5, btnNum6, btnNum7, btnNum8, btnNum9];
 var btnNumObjs = [];
 
-// this creates unique objects with a different Number each object is
-// able to refer to a number-button based on the number it was given
+// This loop assigns each btn ID to a variable that corresponds with its name.
+// It then assigns each of these variables with an object (via constructor).
+// The object constructor is passed the button number that allows it to have
+// an onclick event that responds based on the number given.
 
 for (var i = 0; i < btnNumArr.length; i++) {
   btnNumArr[i] = document.getElementById("btnNum" + i);
@@ -37,13 +31,15 @@ for (var i = 0; i < btnNumArr.length; i++) {
 
 /////////////////////////////////////////////////////////////////////
 
-// these are used to help the computer understand what to do with
-// the next number that is input into the system
+// These variables act as a que for what operation will come next.
+// If "adding" is true, then the next operator/equals pressed will add the numbers.
 
 var adding = false;
 var subtracting = false;
 var multiplying = false;
 var dividing = false;
+
+// This function is for quick-assignment of the que.
 
 function updateOperationStatus(add, sub, mult, div) {
   adding = add;
@@ -54,27 +50,31 @@ function updateOperationStatus(add, sub, mult, div) {
 
 /////////////////////////////////////////////////////////////////////
 
-// This array contains all opperands
-// this array is cleared whenever the clear button is clicked
+// This "storedNum" variable is where the number to be operated on will be stored.
+// "startNewCalc", when true, allows number buttons to clear the output window for fresh calculations.
+// (This only applies when a calculation has been completed. It bypasses the need to clear the window manually via the "clear" button).
 
-var operands = [];
+var storedNum = 0;
+var startNewCalc = false;
 
 /////////////////////////////////////////////////////////////////////
 
-// C O N S T R U C T O R for creating the number button objects
-// (doesnt include operator buttons)
+// This is the constructor for creating the calcuator's number buttons.
+// It assigns a onclick event to each button that adds the button to the output window.
+// These numbers can be added to eachother to create larger numbers before finally being operated on.
 
 function ButtonNum(num) {
   this.num = num;
   this.include = function() {
     var that = this;
     btnNumArr[this.num].onclick = function() {
+      disableOperators(false);
+      if (startNewCalc) {
+        outputScreen.value = "";
+        startNewCalc = false;
+      }
       outputScreen.value = outputScreen.value + that.num;
       adjustTextSize();
-
-      if (adding) {
-        add();
-      }
     }
   }
 }
@@ -112,60 +112,136 @@ function disableAllButtons(value) {
 
 /////////////////////////////////////////////////////////////////////
 
-function operate(operator) {
+// This function is used to perform operations on the stored number.
+// The qued operation determines which of these operations will be ran.
 
+function operate() {
+  if (adding) {
+    outputScreen.value = Number(storedNum) + Number(outputScreen.value);
+  } else if (subtracting) {
+    outputScreen.value = storedNum - Number(outputScreen.value);
+  } else if (multiplying) {
+    outputScreen.value = storedNum * Number(outputScreen.value);
+  } else if (dividing) {
+    outputScreen.value = storedNum / Number(outputScreen.value);
+  }
 }
 
 /////////////////////////////////////////////////////////////////////
 
-function add() {
-  var stored;
-  operands.push(Number(outputScreen.value));
-  stored = operands[0] + operands[1];
-  outputScreen.value = stored;
-  operands = [];
-  operands.push()
+// This is used to disable or enable all operator buttons.
+
+function disableOperators(value) {
+  btnEquals.disabled = value;
+  btnPlus.disabled = value;
+  btnMinus.disabled = value;
+  btnDivide.disabled = value;
+  btnMultiply.disabled = value;
 }
 
 /////////////////////////////////////////////////////////////////////
 
-// on click, clear the output screen and re-enable all buttons if they're disabled
+// When "btnClear" is clicked, clear the output screen and re-enable all buttons if they're disabled.
 
 btnClear.onclick = function() {
   outputScreen.value = "";
   outputScreen.style.fontSize = "4rem";
   if (btnNum0.disabled === true) disableAllButtons(false);
-  operands = [];
+  storedNum = 0;
 }
 
 /////////////////////////////////////////////////////////////////////
 
-// on click, add a decimal number to the screen
+// On click, add a decimal to the output outputScreen number if it isn't empty.
 
 btnDecimal.onclick = function() {
-  outputScreen.value = outputScreen.value + ".";
+  if (outputScreen.value) outputScreen.value = outputScreen.value + ".";
 }
 
 /////////////////////////////////////////////////////////////////////
 
-// on click, push outputScreen number to the operands array
-// (located at the top of file)
+// Whenever an operator button is clicked it checks to see if the storedNum is empty.
+// If so, it updates the storedNum by assigning it to the output screen value.
+// It then clears the output screen for further inputs.
+
+// If the storedNum is not empty, then the operator will perform an operation based on which operator it is.
+// It will then assign the stored number to the new operation value.
+// The "startNewCalc" variable will be set to "true" to allow inputs to clear output screen for the next operations.
+// Finally, the operation que will be set to its corresponding operator (adding, subtracting, multiplying, dividing).
+// When an operation is about to be performed this que is checked, and whichever is true determines which operation will be carried out.
 
 btnPlus.onclick = function() {
-  if (outputScreen.value != "") {
+  disableOperators(true);
+  if (storedNum !== 0) {
+    operate();
+    storedNum = outputScreen.value;
+    startNewCalc = true;
     updateOperationStatus(true, false, false, false);
-    operands.push(Number(outputScreen.value));
+  } else {
+    updateOperationStatus(true, false, false, false);
+    storedNum = outputScreen.value;
     outputScreen.value = "";
-    console.log(operands);
   }
 }
 
+/////////////////////////////////////////////////////////////////////
 
-
+btnMinus.onclick = function() {
+  disableOperators(true);
+  if (storedNum !== 0) {
+    operate();
+    storedNum = outputScreen.value;
+    startNewCalc = true;
+    updateOperationStatus(false, true, false, false);
+  } else {
+    updateOperationStatus(false, true, false, false);
+    storedNum = outputScreen.value;
+    outputScreen.value = "";
+  }
+}
 
 /////////////////////////////////////////////////////////////////////
 
-// on click, run
+btnMultiply.onclick = function() {
+  disableOperators(true);
+  if (storedNum !== 0) {
+    operate();
+    storedNum = outputScreen.value;
+    startNewCalc = true;
+    updateOperationStatus(false, false, true, false);
+  } else {
+    updateOperationStatus(false, false, true, false);
+    storedNum = outputScreen.value;
+    outputScreen.value = "";
+  }
+}
 
+/////////////////////////////////////////////////////////////////////
+
+btnDivide.onclick = function() {
+  disableOperators(true);
+  if (storedNum !== 0) {
+    operate();
+    storedNum = outputScreen.value;
+    startNewCalc = true;
+    updateOperationStatus(false, false, false, true);
+  } else {
+    updateOperationStatus(false, false, false, true);
+    storedNum = outputScreen.value;
+    outputScreen.value = "";
+  }
+}
+
+/////////////////////////////////////////////////////////////////////
+
+btnEquals.onclick = function() {
+  if (outputScreen.value) {
+    if (storedNum) {
+      operate();
+      startNewCalc = true;
+      updateOperationStatus(false, false, false, false);
+    }
+  }
+}
 
 /////////////////////////////////////////////////////////////////////
